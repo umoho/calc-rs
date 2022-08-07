21c31289099c5c2167f6d26878f0854ea1ba41c0
umoho <my.umoho@hotmail.com>
2022-08-07 11:42:07 +0800

    simplify `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ mod interpreter;
 enum Mode { Calculate, Lexeme, Tree }
 
 fn exec(input: &str, mode: Mode) -> String {
-    match panic::catch_unwind(|| {  // head of `match`
+    let result = panic::catch_unwind(|| {
         let token_stream = get_tokens(input);
         let mut parser = Parser::new(token_stream.clone());
         match mode {
@@ -20,11 +20,11 @@ fn exec(input: &str, mode: Mode) -> String {
             Mode::Lexeme => show_tokens(&token_stream),
             Mode::Tree => format!("{:#?}", parser.parse()),
         }
-    })
-    {  // body of `match`
+    });
+    match result {
         Ok(s) => s,
-        Err(_e) => {
-            format!("@error: {:#?}", _e)  // todo: more user-friendly
+        Err(_) => {
+            format!("@error")  // todo: more user-friendly
         }
     }
 }
